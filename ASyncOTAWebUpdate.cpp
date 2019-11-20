1f65d43a86ffe44fcf272ecd95ff2b66eabc0341
Paul Andrews <judge2005@gmail.com>
2019-11-19 23:04:56 -0500

    Tested ESP32 code

---
+++ ASyncOTAWebUpdate.cpp
@@ -0,0 +1,98 @@
+/*
+ * ASyncOTAWebUpdate.cpp
+ *
+ *  Created on: Nov 19, 2019
+ *      Author: mpand
+ */
+
+#include <ASyncOTAWebUpdate.h>
+
+ASyncOTAWebUpdate::~ASyncOTAWebUpdate() {
+}
+
+void ASyncOTAWebUpdate::init(AsyncWebServer &server, const char *path, pUpdateFormFunc updateFormCallback, pUpdatingFunc updatingCallback) {
+	this->updateFormCallback = updateFormCallback;
+	this->updatingCallback = updatingCallback;
+	// Dontcha just love C++?
+	using namespace std::placeholders;
+	server.on(path, HTTP_GET,
+			std::bind(&ASyncOTAWebUpdate::handleUpdateGet, this, _1)
+	).setFilter(ON_STA_FILTER);
+	server.on(path, HTTP_POST,
+			std::bind(&ASyncOTAWebUpdate::handleUpdatePost, this, _1),
+			std::bind(&ASyncOTAWebUpdate::handleUpdateUpload, this, _1, _2, _3, _4, _5, _6)
+	).setFilter(ON_STA_FILTER);
+#ifdef ESP32
+	Update.onProgress(std::bind(&ASyncOTAWebUpdate::printProgress, this, _1, _2));
+#endif
+}
+
+void ASyncOTAWebUpdate::printProgress(size_t prg, size_t sz) {
+	if (contentLen != 0) {
+		Serial.printf("Progress: %d%%\n", (prg*100)/contentLen);
+	} else {
+		Serial.printf("Progress: %d of %d\n", prg, sz);
+	}
+}
+
+void ASyncOTAWebUpdate::handleUpdateGet(AsyncWebServerRequest *request) {
+	if(!request->authenticate(username, password)) {
+	    return request->requestAuthentication();
+	}
+	updateFormCallback(request);
+}
+
+void ASyncOTAWebUpdate::handleUpdatePost(AsyncWebServerRequest *request) {
+    AsyncResponseStream *response = request->beginResponseStream("text/html");
+    updatingCallback(response, update.hasError());
+    request->client()->onDisconnect([](void *arg, AsyncClient *client){
+        ESP.restart();
+    });
+    request->send(response);
+}
+
+void ASyncOTAWebUpdate::handleUpdateUpload(AsyncWebServerRequest *request, const String& filename, size_t index, uint8_t *data, size_t len, bool final) {
+	contentLen = 0;
+
+	if (!request->authenticate(username, password)) {
+		return request->requestAuthentication();
+	}
+
+	if (!index) {
+		Serial.println("update");
+		contentLen = request->contentLength();
+		// if filename includes spiffs, update the spiffs partition
+		int cmd = U_FLASH;
+		if (filename.indexOf("spiffs") > -1) {
+			cmd = U_SPIFFS;
+			SPIFFS.end();
+		}
+
+		Serial.printf("Loading %s\n", filename.c_str());
+
+#ifdef ESP8266
+		update.runAsync(true);
+		if (!update.begin(contentLen, cmd)) {
+#else
+		if (!update.begin(UPDATE_SIZE_UNKNOWN, cmd)) {
+#endif
+			update.printError(Serial);
+		}
+	}
+
+	if (update.write(data, len) != len) {
+		update.printError(Serial);
+#ifdef ESP8266
+	} else {
+		Serial.printf("Progress: %d%%\n", (update.progress()*100)/update.size());
+#endif
+	}
+
+	if (final) {
+		if (!update.end(true)) {
+			update.printError(Serial);
+		}
+	}
+}
+
+
---
+++ ASyncOTAWebUpdate.h
@@ -0,0 +1,41 @@
+/*
+ * ASyncOTAWebUpdate.h
+ *
+ *  Created on: Nov 19, 2019
+ *      Author: mpand
+ */
+
+#ifndef LIBRARIES_ASYNCOTAWEBUPDATE_ASYNCOTAWEBUPDATE_H_
+#define LIBRARIES_ASYNCOTAWEBUPDATE_ASYNCOTAWEBUPDATE_H_
+
+#include "ESPAsyncWebServer.h"
+#include "SPIFFS.h"
+#include "Update.h"
+
+class ASyncOTAWebUpdate {
+public:
+	typedef void (*pUpdateFormFunc)(AsyncWebServerRequest *request);
+	typedef void (*pUpdatingFunc)(AsyncResponseStream *response, boolean hasError);
+
+	ASyncOTAWebUpdate(UpdateClass &update, const char *username, const char *password) :
+		update(update), username(username), password(password)
+	{
+	}
+	virtual ~ASyncOTAWebUpdate();
+	void init(AsyncWebServer &server, const char *path, pUpdateFormFunc updateFormCallback, pUpdatingFunc updatingCallback);
+
+private:
+	int contentLen;
+	void handleUpdateGet(AsyncWebServerRequest *request);
+	void handleUpdatePost(AsyncWebServerRequest *request);
+	void handleUpdateUpload(AsyncWebServerRequest *request, const String& filename, size_t index, uint8_t *data, size_t len, bool final);
+	void printProgress(size_t prg, size_t sz);
+
+	UpdateClass &update;
+	pUpdateFormFunc updateFormCallback;
+	pUpdatingFunc updatingCallback;
+	const char *username;
+	const char *password;
+};
+
+#endif /* LIBRARIES_ASYNCOTAWEBUPDATE_ASYNCOTAWEBUPDATE_H_ */
